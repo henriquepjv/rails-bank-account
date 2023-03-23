e3e29f8872bb2caf00e56fe987169742a3538a7a
Henrique Panham <henriquepjv@hotmail.com>
2023-03-23 12:47:46 -0300

    Add create user view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save!
+      redirect_to action: 'show', id: @user.id
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "dashboard#index"
+
+  post '/login', to: 'dashboard#login', as: 'login'
+
+  resources :users
 end
